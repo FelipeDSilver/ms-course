548375f5413faf9f421188cddda1fd8c62574109
FelipeDSilver <felipedonsilva@hotmail.com>
2025-11-02 09:22:33 -0300

    hr-worker implementation

---
+++ src/main/java/com/devsuperior/hr_worker/entities/Worker.java
@@ -0,0 +1,62 @@
+package com.devsuperior.hr_worker.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Worker(){
+
+    }
+
+    public Worker(Long id, String name, Double dailyincome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyincome() {
+        return dailyIncome;
+    }
+
+    public void setDailyincome(Double dailyincome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(id, worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
